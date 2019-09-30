02cec878551cd600395ec7f66f24d0e85e1caffb
leonidstr <leonid.stryzhevskyi@firstbridge.io>
2019-09-30 19:36:01 +0300

    APL-940_fix getting apl/pax balance test-2

--- apl-exec/src/main/java/com/apollocurrency/aplwallet/apl/exec/Apollo.java
+++ apl-exec/src/main/java/com/apollocurrency/aplwallet/apl/exec/Apollo.java
@@ -200,6 +200,8 @@ public class Apollo {
 //        }
         System.setProperty("apl.runtime.mode", args.serviceMode ? "service" : "user");
         System.setProperty("javax.net.ssl.trustStore", "cacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
 
 //cheat classloader to get access to package resources
         ConfPlaceholder ph = new ConfPlaceholder();
